582746dd99830e0eb65b63d9fc04d0de495292e3
Christopher <chris@christophermoore.website>
2019-11-16 12:59:43 +1000

    Public Beta Version 1.2
    ![channels4_banner|690x114](https://i.ibb.co/CHMD8y6/channels4-banner.jpg)
    # Inferno Collection: Whitelist
    [![Build Status](https://travis-ci.com/inferno-collection/Whitelist.svg?branch=master)](https://travis-ci.com/inferno-collection/Whitelist)
    
    __Public Beta Version 1.2 Beta__
    
    The Inferno Whitelist is specifically designed for server owners looking for a way to allow other people (such as Admins, Mods, etc.) to add and remove people for their server whitelist, without needing to give them Box/FTP/Control Panel access. Inferno Whitelist, though the use of PasteBin and it's free API, provide server owners a way of doing this. In a nutshell, server owners create a PasteBin whitelist, allow other people to edit said whitelist, then the Inferno Whitelist resource automatically collects the whitelist every X minutes, where X is configurable.
    
    **Some assembly is required, make sure to read the [installation guide](https://github.com/inferno-collection/Whitelist/wiki/Installation-Guide)!**
    
    Presently, the following can be customized:
    - How often the whitelist automatically refreshes.
    
    ***
    
    > Download Link
    
    https://github.com/inferno-collection/Whitelist/releases
    
    ***
    Check out the Wiki for an [installation guide](https://github.com/inferno-collection/Whitelist/wiki/Installation-Guide), as well as to find out how to use the resource.
    
    If you have any troubles, suggestions, feedback, etc, please [check the Wiki](https://github.com/inferno-collection/Whitelist/wiki) and/or [create a new issue](https://github.com/inferno-collection/Whitelist/issues/new/choose) on GitHub.
    
    Thank you to our Alpha Testers, and their respective servers and players, for testing this resource:
    * LurkingSpirit#8338
    * Tom | DutchGamingsHD15#7193
    * J. Hughesman#6922
    * Ovist#9081
    * FamalamJamalam#0001
    * Beerasaurus - Rex#9371
    * Trevor Cory#7451
    
    > The Inferno Collection Team
    * @ChristopherM
    * @Scott_UK
    * @FrozenN00b
    ***
    Interested in keeping up-to-date with what we are working on? [Check out our roadmap](https://inferno-collection.com/roadmap).
    
    Co-Authored-By: scottuk <scottuk@users.noreply.github.com>
    Co-Authored-By: Daniel Hawton <daniel@hawton.org>

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# Whitelist
-An external whitelist for FiveM servers.
+![channels4_banner|690x114](https://i.ibb.co/CHMD8y6/channels4-banner.jpg) 
+# Inferno Collection: Whitelist
+[![Build Status](https://travis-ci.com/inferno-collection/Whitelist.svg?branch=master)](https://travis-ci.com/inferno-collection/Whitelist)
+
+__Public Beta Version 1.2 Beta__
+
+The Inferno Whitelist is specifically designed for server owners looking for a way to allow other people (such as Admins, Mods, etc.) to add and remove people for their server whitelist, without needing to give them Box/FTP/Control Panel access. Inferno Whitelist, though the use of PasteBin and it's free API, provide server owners a way of doing this. In a nutshell, server owners create a PasteBin whitelist, allow other people to edit said whitelist, then the Inferno Whitelist resource automatically collects the whitelist every X minutes, where X is configurable.
+
+**Some assembly is required, make sure to read the [installation guide](https://github.com/inferno-collection/Whitelist/wiki/Installation-Guide)!**
+
+Presently, the following can be customized:
+- How often the whitelist automatically refreshes.
+
+***
+
+> Download Link
+
+https://github.com/inferno-collection/Whitelist/releases 
+
+***
+Check out the Wiki for an [installation guide](https://github.com/inferno-collection/Whitelist/wiki/Installation-Guide), as well as to find out how to use the resource.
+
+If you have any troubles, suggestions, feedback, etc, please [check the Wiki](https://github.com/inferno-collection/Whitelist/wiki) and/or [create a new issue](https://github.com/inferno-collection/Whitelist/issues/new/choose) on GitHub.
+
+Thank you to our Alpha Testers, and their respective servers and players, for testing this resource:
+* LurkingSpirit#8338
+* Tom | DutchGamingsHD15#7193
+* J. Hughesman#6922
+* Ovist#9081
+* FamalamJamalam#0001
+* Beerasaurus - Rex#9371
+* Trevor Cory#7451
+
+> The Inferno Collection Team
+* @ChristopherM
+* @Scott_UK 
+* @FrozenN00b
+***
+Interested in keeping up-to-date with what we are working on? [Check out our roadmap](https://inferno-collection.com/roadmap).
---
+++ [inferno-collection]/inferno-whitelist/server.lua
@@ -0,0 +1,132 @@
+-- Inferno Collection Whitelist Version 1.2 Beta
+--
+-- Copyright (c) 2019, Christopher M, Inferno Collection. All rights reserved.
+--
+-- This project is licensed under the following:
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to use, copy, modify, and merge the software, under the following conditions:
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. THE SOFTWARE MAY NOT BE SOLD.
+--
+
+--
+-- Resource Configuration
+-- PLEASE RESTART SERVER AFTER MAKING CHANGES TO THIS CONFIGURATION
+--
+
+local Config = {} -- Do not edit this line
+-- Time in minutes between whitelist refresh intervals
+Config.RefreshTime = 30
+-- The URL of your PasteBin
+-- Should looke like: https://pastebin.com/eyba7r
+Config.PasteBinURL = ""
+-- API Dev Key for PasteBin.com
+-- "Your Unique Developer API Key" from https://pastebin.com/api
+-- Should look like: d460f1e7a0ba23662fb8d56g77g7147y
+Config.APIDevKey = ""
+-- API User Key for PasteBin.com
+-- Use the API Dev Key, and your username and password here: https://pastebin.com/api/api_user_key.html
+-- Should look like: b4ca9es8cadae90fge7ghfy8361c9f4m
+Config.APIUserKey = ""
+
+--
+--		Nothing past this point needs to be edited, all the settings for the resource are found ABOVE this line.
+--		Do not make changes below this line unless you know what you are doing!
+--
+
+local Whitelist = false
+-- Removes everything from the URL except the key at the end
+Config.APIPasteKey = Config.PasteBinURL:match("([^/]+)$")
+
+AddEventHandler("onResourceStart", function(Resource)
+    if (GetCurrentResourceName() == Resource) then
+        Citizen.CreateThread(function()
+            while true do
+                PerformHttpRequest("https://pastebin.com/api/api_raw.php", function (Code, Body)
+                    -- If not HTTP Code 200 OK
+                    if Code ~= 200 then
+                        print("===================================================================")
+                        print("==========================CRITICAL ERROR===========================")
+                        print("Inferno-Whitelist was NOT able to load the whitelist file: no playe")
+                        print("rs will be allowed to join the server until this issue is fixed.")
+                        print("")
+                        print("Error Code Received: " .. Code)
+                        print("===================================================================")
+                    -- If HTTP Code 200 OK received
+                    else
+                        Whitelist = json.decode(Body)
+                        print("===================================================================")
+                        print("=========================Inferno-Whitelist=========================")
+                        print("The whitelist file has been loaded successfully! Players that join ")
+                        print("will now checked upon entry to the server. Have a nice day!")
+                        print("===================================================================")
+                    end
+                -- Send a POST request to the PasteBin API with the provided details from the config
+                end, "POST", "api_option=show_paste&api_user_key=" .. Config.APIUserKey .. "&api_dev_key=" .. Config.APIDevKey .. "&api_paste_key=" .. Config.APIPasteKey)
+
+                Citizen.Wait(Config.RefreshTime * 60000)
+                print("===================================================================")
+                print("=========================Inferno-Whitelist=========================")
+                print("Collecting lastest version of whitelist... Please stand by.")
+                print("===================================================================")
+            end
+        end)
+    end
+end)
+
+AddEventHandler("playerConnecting", function(Name, Kick, Deferrals)
+    -- Defer the client while we start the checking process
+    Deferrals.defer()
+    -- The cline only sees this message if they are deferred for more than a few seconds
+    Deferrals.update("Welcome " .. Name .. ", we are checking your whitelist status, please stand by.")
+
+    local IDs = GetPlayerIdentifiers(source)
+
+    if Whitelist then
+        local Whitelisted = false
+
+        -- Fast-track method
+        -- As long as identifers are in lowercase in the `whitelist.json` file, this method will work, and is much faster than the other method.
+        for _, ID in pairs(IDs) do
+            if Whitelist[ID] then
+                Whitelisted = true
+                print(Name .. " was approved with ID '" .. tostring(ID) .. "' using the Fast-Track method.")
+                break
+            end
+        end
+
+        -- Loop method
+        -- If user cannot be found using the fast-track method, the whitelist will be searched line-by-line. Not stressful for the server, just slow.
+        if not Whitelisted then
+            for Entry, _ in pairs(Whitelist) do
+                for _, ID in pairs(IDs) do
+                    if ID:lower() == Entry:lower() then
+                        Whitelisted = true
+                        print("===================================================================")
+                        print("=============================ATTENTION=============================")
+                        print(Name .. " was approved with ID '" .. tostring(ID) .. "' using the")
+                        print("Loop method, that's bad! Please check their whitelist entry/s, and ")
+                        print("ensure it is in all lowercase, so they can join faster next time!")
+                        print("===================================================================")
+                        break
+                    end
+                end
+            end
+        end
+
+        if Whitelisted then
+            -- Allow client access to server
+            Deferrals.done()
+        else
+            -- Deny the client access to the server
+            Deferrals.done("Sorry " .. Name .. ", it appears you are not whitelisted. If you believe this is in error, contact server staff.")
+        end
+
+    else
+        Deferrals.done("Sorry " .. Name .. ", the server is experiencing some issues at the moment, could you please inform the staff? Thank you!")
+        print("===================================================================")
+        print("==========================CRITICAL ERROR===========================")
+        print("Inferno-Whitelist was NOT able to load the whitelist file: no playe")
+        print("rs will be allowed to join the server until this issue is fixed.")
+        print("===================================================================")
+    end
+end)
